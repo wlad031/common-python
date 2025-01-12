3ccddacd3335cecaf4fd8e531a743bc5903840d3
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2025-01-12 13:39:55 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="common_python",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
+
